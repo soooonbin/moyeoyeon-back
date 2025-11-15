e4224d4c8425d44ff9e6b08c861b956f6c7123ed
soooonbin <dlvtoekf4@naver.com>
2025-11-15 12:47:35 +0900

    Initial commit

---
+++ src/main/java/com/moyeo/MoyeoApplication.java
@@ -0,0 +1,13 @@
+package com.moyeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoyeoApplication.class, args);
+	}
+
+}
